fff20aa1a84af12b47aa2af121536da957083c31
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-12-09 01:46:33 -0500

    layout ajouter

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,108 +1,129 @@
 <template>
   <div :class="[backgroundClass]">
     <BackgroundContainer>
-      <div class="grid grid-cols-12">
-
-        <div v-if="livres.length === 0" class="col-span-12">
-          <div class="grid grid-cols-2 pt-12 h-screen">
-            <h1 class="text-8xl m-4 text-left animate-fade-in-down">
-              Votre collection
-            </h1>
-            <p class="text-8xl m-4 self-end text-right animate-fade-in-up">
-              est vide.
-            </p>
-          </div>
+      <div v-if="livres.length === 0">
+        <div class="grid grid-cols-2 pt-12 h-screen">
+          <h1 class="text-8xl m-4 text-left animate-fade-in-down">
+            Votre collection
+          </h1>
+          <p class="text-8xl m-4 self-end text-right animate-fade-in-up">
+            est vide.
+          </p>
         </div>
+      </div>
 
-        <div v-else class="flex flex-wrap justify-center gap-6 pt-12 h-screen">
-          <CarteLivre
-            v-for="livre in livres"
-            :key="livre.id"
-            :livre="livre"
-            class="relative z-10"
-          />
-        </div>
+      <div v-else class="grid grid-col-3 gap-6 pt-12 h-screen">
 
+        <h1 class="text-8xl m-4 text-left animate-fade-in-down">
+          Votre collection
+        </h1>
+
+        <splide :options="{
+                type: 'slide',
+                perPage: 4,
+                autoplay: false,
+                gap: '1rem',
+                pagination: true,
+                arrows: true,
+                direction: 'ltr',
+                breakpoints: {
+                  1024: { perPage: 3 },
+                  768: { perPage: 2 },
+                  640: { perPage: 1 },
+                },
+              }">
+          <SplideSlide v-for="livre in livres" :key="livre.id">
+            <CarteLivre :livre="livre" />
+          </SplideSlide>
+        </Splide>
+
+        <p class="text-8xl m-4 self-end text-right animate-fade-in-up">
+          contient {{ livres.length }} livres.
+        </p>
       </div>
     </BackgroundContainer>
   </div>
 </template>
 
 <script setup>
-  import { ref, computed, onMounted } from "vue";
-  import CarteLivre from "@/components/CarteLivre.vue";
-  import { utiliserCollection } from "@/composables/utiliserCollection";
-  import BackgroundContainer from "@/components/BackgroundContainer.vue";
+import { ref, computed, onMounted } from "vue";
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+import CarteLivre from "@/components/CarteLivre.vue";
+import { utiliserCollection } from "@/composables/utiliserCollection";
+import BackgroundContainer from "@/components/BackgroundContainer.vue";
 
-  const { livres } = utiliserCollection();
+const { livres } = utiliserCollection();
 
-  // Vérifiez la longueur de livres
-  onMounted(() => {
-    console.log("Nombre de livres:", livres.value.length);
-  });
+// Vérifiez la longueur de livres
+onMounted(() => {
+  console.log("Nombre de livres:", livres.value.length);
+});
 
-  const backgroundClass = computed(() => {
-    const className =
-      livres.value.length === 0 ? "background-empty" : "background-full";
-    console.log("Background class:", className); // Vérifiez la valeur de backgroundClass
-    return className;
-  });
+const backgroundClass = computed(() => {
+  const className =
+    livres.value.length === 0 ? "background-empty" : "background-full";
+  console.log("Background class:", className); // Vérifiez la valeur de backgroundClass
+  return className;
+});
 </script>
 
 <style scoped>
-  .background-empty {
-    background-image: url("../../public/bibliotheque-vide.jpg");
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: -1;
+.background-empty {
+  background-image: url("public/bibliotheque-vide.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+
+.background-full {
+  background-image: url("public/bibliotheque.avif");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+
+@keyframes fadeInDown {
+  from {
+    opacity: 0;
+    transform: translateY(-12rem);
   }
 
-  .background-full {
-    background-image: url("../../public/bibliotheque.webp");
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: -1;
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translateY(12rem);
   }
 
-  @keyframes fadeInDown {
-    from {
-      opacity: 0;
-      transform: translateY(-12rem);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
+  to {
+    opacity: 1;
+    transform: translateY(0);
   }
+}
 
-  @keyframes fadeInUp {
-    from {
-      opacity: 0;
-      transform: translateY(12rem);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
+.animate-fade-in-down {
+  animation: fadeInDown 2s ease-out;
+}
 
-  .animate-fade-in-down {
-    animation: fadeInDown 2s ease-out;
-  }
-
-  .animate-fade-in-up {
-    animation: fadeInUp 2s ease-out;
-  }
+.animate-fade-in-up {
+  animation: fadeInUp 2s ease-out;
+}
 </style>
--- src/views/AjoutLivre.vue
+++ src/views/AjoutLivre.vue
@@ -1,49 +1,51 @@
 <template>
   <div class="background">
     <BackgroundContainer>
-      <div class="mx-auto p-4 pt-24 grid grid-cols-12">
-
-        <div class="col-span-12">
-          <input
-            v-model="requete"
-            @input="chercherLivres"
-            placeholder="Rechercher un livre par titre"
-            class="h-fit text-black w-full p-3 mb-6 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-
-
-        <div class="col-span-12 grid grid-row-3 stretch">
-          <p class="text-8xl m-4 self-start text-left animate-fade-in-down">Ajouter un livre</p>
-
-          <div
-            v-if="resultats.length > 0"
-            class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
-          >
-            <div
-              v-for="livre in resultats"
-              :key="livre.id"
-              @click="ajouterLivreEtEffacerResultats(livre)"
-              class="carte-livre p-6 bg-white shadow-lg rounded-lg text-center cursor-pointer hover:shadow-xl transition-shadow duration-300"
-            >
-              <img
-                :src="livre.volumeInfo.imageLinks?.thumbnail"
-                :alt="livre.volumeInfo.title"
-                class="w-full h-48 mb-4 rounded object-cover mx-auto"
-              />
-              <h2 class="text-xl font-semibold mb-2">
-                {{ livre.volumeInfo.title }}
-              </h2>
-            </div>
+      <div class="mx-auto grid grid-cols-12">
+        <div class="col-span-12 h-screen">
+          <div class="pt-12">
+            <input v-model="requete" @input="chercherLivres" placeholder="Rechercher un livre par titre"
+              class="h-fit text-black w-full p-3 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
 
-          <p v-else class="text-3xl m-4 text-center">
-            Aucun résultat trouvé
-          </p>
+          <div class="grid grid-row-3 h-full">
+            <h1 class="text-8xl m-4 flex flex-row flex-wrap animate-fade-in-down">
+              Ajouter un livre
+            </h1>
 
-          <p class="text-8xl m-4 self-end text-right animate-fade-in-up">
-            à la collection
-          </p>
+            <div v-if="resultats.length > 0" class="container">
+              <splide :options="{
+                type: 'loop',
+                perPage: 4,
+                autoplay: false,
+                gap: '1rem',
+                pagination: true,
+                arrows: true,
+                direction: 'ltr',
+                breakpoints: {
+                  1024: { perPage: 3 },
+                  768: { perPage: 2 },
+                  640: { perPage: 1 },
+                },
+              }">
+                <splide-slide v-for="livre in resultats" :key="livre.id" class="">
+                  <div @click="ajouterLivreEtEffacerResultats(livre)"
+                    class="p-6 bg-white shadow-lg rounded-lg text-center cursor-pointer hover:shadow-xl transition-shadow duration-300">
+                    <img :src="livre.volumeInfo.imageLinks?.thumbnail || '/placeholder.jpg'" 
+                      :alt="livre.volumeInfo.title" class="h-48 mb-4 rounded object-cover mx-auto" />
+                    <h2 class="text-xl font-semibold mb-2">{{ livre.volumeInfo.title }}</h2>
+                  </div>
+                </splide-slide>
+              </splide>
+            </div>
+              <p v-else class="text-3xl m-4 text-center self-center">
+                Aucun résultat trouvé
+              </p>
+
+            <p class="text-8xl mb-32 self-end text-right animate-fade-in-up">
+              à la collection
+            </p>
+          </div>
         </div>
       </div>
     </BackgroundContainer>
@@ -51,71 +53,85 @@
 </template>
 
 <script setup>
-  import BackgroundContainer from "@/components/BackgroundContainer.vue";
-  import { ref } from "vue";
-  import { chercherLivresGoogle } from "@/composables/fetchGoogleBooks";
-  import { utiliserCollection } from "@/composables/utiliserCollection";
+import BackgroundContainer from "@/components/BackgroundContainer.vue";
+import { ref } from "vue";
+import { chercherLivresGoogle } from "@/composables/fetchGoogleBooks";
+import { utiliserCollection } from "@/composables/utiliserCollection";
+import {Splide, SplideSlide } from "@splidejs/vue-splide";
+import '@splidejs/vue-splide/css';
 
-  const requete = ref("");
-  const resultats = ref([]);
-  const { ajouterLivre } = utiliserCollection();
+const requete = ref("");
+const resultats = ref([]);
+const { ajouterLivre } = utiliserCollection();
 
-  const chercherLivres = async () => {
-    if (requete.value.trim() === "") {
-      resultats.value = [];
-      return;
-    }
-    resultats.value = await chercherLivresGoogle(requete.value);
-  };
-
-  const ajouterLivreEtEffacerResultats = (livre) => {
-    ajouterLivre(livre);
+const chercherLivres = async () => {
+  if (requete.value.trim() === "") {
     resultats.value = [];
-    requete.value = "";
-  };
+    return;
+  }
+  resultats.value = await chercherLivresGoogle(requete.value);
+};
+
+const ajouterLivreEtEffacerResultats = (livre) => {
+  ajouterLivre(livre);
+  resultats.value = [];
+  requete.value = "";
+};
 </script>
 
 <style scoped>
-  @keyframes fadeInDown {
-    from {
-      opacity: 0;
-      transform: translateY(-12rem);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
+@keyframes fadeInDown {
+  from {
+    opacity: 0;
+    transform: translateY(-12rem);
   }
 
-  @keyframes fadeInUp {
-    from {
-      opacity: 0;
-      transform: translateY(12rem);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translateY(12rem);
   }
 
-  .animate-fade-in-down {
-    animation: fadeInDown 2s ease-out;
+  to {
+    opacity: 1;
+    transform: translateY(0);
   }
+}
 
-  .animate-fade-in-up {
-    animation: fadeInUp 2s ease-out;
-  }
+.animate-fade-in-down {
+  animation: fadeInDown 2s ease-out;
+}
 
-  .background {
-    background-image: url("../../public/bibliotheque-2.jpg");
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: -1;
-  }
+.animate-fade-in-up {
+  animation: fadeInUp 2s ease-out;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.background {
+  background-image: url("/bibliotheque-2.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
